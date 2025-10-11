8b4739df0401a24ae0eed6c7ba159b1573ad15e5
edigitify <edigitify@edigitifys-MacBook-Air-2.local>
2025-10-11 15:12:22 +0530

    Nav Fix

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -18,12 +18,12 @@ export default function NavBar() {
   const mainPages = [
     "/",
     "/projects",
-    "/product",
+    "/products",
     "/career",
     
     "https://www.dezainahub.com/",
     "https://www.dezainahub.com/projects",
-    "https://www.dezainahub.com/product",
+    "https://www.dezainahub.com/products",
     "https://career.dezainahub.com",
     
   ]
